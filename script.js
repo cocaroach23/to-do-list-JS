70d6dffd07b2b53f33082df962893f9bf8c1c420
cocaroach23 <cocaroach23@gmail.com>
2023-04-06 07:42:15 +0300

    в браузере не работает script

---
+++ script.js
@@ -0,0 +1,26 @@
+let username = prompt ("Введите своё имя в дательной падеже")
+
+let myTitle = document.getElementById('title')
+
+if (username) {
+    myTitle.innerHTML = "Задачи для " + username
+} else {
+    myTitle.innerHTML = "Неавторизороанный пользователь"
+}
+
+let number = 0
+
+for (let number = 0; number < 5; number++){
+    alert(number)
+}
+
+function readTask() {
+    let myContainer = document.getElementById("container")
+    let myInput = document.getElementById("input")
+
+    let value = myInput.value
+
+    number++
+
+    myContainer.innerHTML = myContainer.innerHTML + "<p>" + number  + value + "</p>"
+}
