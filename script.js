8fefc439fb528e25410daa17b5bec24b76124073
cocaroach23 <cocaroach23@gmail.com>
2023-04-06 11:28:36 +0300

    скрипт работает

--- script.js
+++ script.js
@@ -10,22 +10,28 @@ if (username) {
 
 let number = 0
 
-for (let number = 0; number < 5; number++){
-    //alert(number)
-}
+
 
 function readTask() {
     let myContainer = document.getElementById("container")
     let myInput = document.getElementById("input")
 
     let value = myInput.value
-
+    myInput.value = ""
     
-    number++
+    //number++
+
+    if(number < 5) {
+        number++
+        myContainer.innerHTML = myContainer.innerHTML + "<p>" + number + "."  + value + "</p>"
+        
+    } else {
+        confirm("Колличество записей ограничено до 5")
+    }
 
     /*for (let number = 0; number < 5; number++){
         alert(number)
     }*/
 
-    myContainer.innerHTML = myContainer.innerHTML + "<p>" + number  + value + "</p>"
+    //myContainer.innerHTML = myContainer.innerHTML + "<p>" + number + "."  + value + "</p>"
 }
